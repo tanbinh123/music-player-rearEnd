2572211801c10522e8fb2dbc702117b5ab60e389
HelloZXB <Hello16369@163.com>
2020-10-24 22:29:54 +0800

    application.properties

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # MusicPlayer
 Music player based on SpringBoot and Vue
 
-# 1
-
-十多个飞
+### 系统环境配置
+* 系统开发平台
+    - `JDK1.8 + Windows10 + Maven3.6.1`
+* 开发语言
+    - `JavaEE + Vue2.0`
+* 后端框架
+    - `SpringBoot2.x`
+* 前端框架
+    - `Vue2.9.6`
+* 数据库和工具
+    - `MySQL5.7`
+    - `Navicat`
+* 开发工具
+    - `IntelliJ IDEA`
--- src/main/java/com/javaclimb/music/MusicApplication.java
+++ src/main/java/com/javaclimb/music/MusicApplication.java
@@ -3,11 +3,16 @@ package com.javaclimb.music;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * 热更新 热加载
+ * 1. Ctrl+Shift+A --> 搜索registry, 找到Registry..., 找到compiler.automake.allow.when.app.running, 然后勾选
+ * 2. 执行快捷键Ctrl+F9才执行热加载
+ * */
 @SpringBootApplication
 public class MusicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApplication.class, args);
+    }
 
 }
