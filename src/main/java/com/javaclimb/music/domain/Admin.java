67204eb8c886ff0261e23f98f256d9d20f505149
HelloZXB <Hello16369@163.com>
2020-10-26 11:32:25 +0800

    设置数据库管理员

---
+++ src/main/java/com/javaclimb/music/domain/Admin.java
@@ -0,0 +1,34 @@
+package com.javaclimb.music.domain;
+
+import java.io.Serializable;
+
+// 管理员
+public class Admin implements Serializable {
+    private Integer id; // 主键
+    private String name; // 账号
+    private String password; // 密码
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
