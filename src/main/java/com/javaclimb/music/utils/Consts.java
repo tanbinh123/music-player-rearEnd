4337fbc5c6718908d5badd646c9d6ac054eb24c0
HelloZXB <Hello16369@163.com>
2020-10-26 13:04:16 +0800

    判断用户名密码是否输入错误

---
+++ src/main/java/com/javaclimb/music/utils/Consts.java
@@ -0,0 +1,7 @@
+package com.javaclimb.music.utils;
+
+public class Consts { // 常量
+    public static final String NAME = "name"; // 登录名
+    public static final String CODE = "code"; // 返回码
+    public static final String MSG = "msg"; // 返回信息
+}
