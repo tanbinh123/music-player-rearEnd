9e2419b34f5e3adaa6c5b778cd0e10fc292ad4c6
HelloZXB <Hello16369@163.com>
2020-10-28 11:06:06 +0800

    歌曲管理-SongController.java/更改FileConfig文件

---
+++ src/main/java/com/javaclimb/music/config/FileConfig.java
@@ -0,0 +1,36 @@
+package com.javaclimb.music.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 定位各种文件和头像地址
+@Configuration
+public class FileConfig implements WebMvcConfigurer {
+
+    @Override
+
+    // 歌手头像地址
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/singerPic/**").addResourceLocations(
+                "file:" + System.getProperty("user.dir") + System.getProperty("file.separator") + "img"
+                        + System.getProperty("file.separator") + "singerPic" + System.getProperty("file.separator")
+        );
+
+
+        //歌单图片地址
+        registry.addResourceHandler("/img/songListPic/**").addResourceLocations(
+                "file:" + System.getProperty("user.dir") + System.getProperty("file.separator") + "img"
+                        + System.getProperty("file.separator") + "songListPic" + System.getProperty("file.separator")
+        );
+        //歌曲图片地址
+        registry.addResourceHandler("/img/songPic/**").addResourceLocations(
+                "file:" + System.getProperty("user.dir") + System.getProperty("file.separator") + "img"
+                        + System.getProperty("file.separator") + "songPic" + System.getProperty("file.separator")
+        );
+        //歌曲地址
+        registry.addResourceHandler("/song/**").addResourceLocations(
+                "file:" + System.getProperty("user.dir") + System.getProperty("file.separator") + "song" + System.getProperty("file.separator")
+        );
+    }
+}
