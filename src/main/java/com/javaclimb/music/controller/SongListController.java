ab89d2ef40f16266fa780af58c1d8c9986eff130
HelloZXB <Hello16369@163.com>
2020-10-29 19:57:15 +0800

    歌单管理-controller层

---
+++ src/main/java/com/javaclimb/music/controller/SongListController.java
@@ -0,0 +1,157 @@
+package com.javaclimb.music.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.javaclimb.music.domain.SongList;
+import com.javaclimb.music.service.SongListService;
+import com.javaclimb.music.utils.Consts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+// 歌单控制类
+@RestController
+@RequestMapping("/songList")
+
+public class SongListController {
+    @Autowired
+    private SongListService songListService;
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public Object addSongList(HttpServletRequest request) { // 添加歌单
+        JSONObject jsonObject = new JSONObject();
+        String title = request.getParameter("title").trim(); // 标题
+        String pic = request.getParameter("pic").trim(); // 歌单图片
+        String introduction = request.getParameter("introduction").trim(); // 简介
+        String style = request.getParameter("style").trim(); // 风格
+
+        // 保存到歌单的对象中
+        SongList songList = new SongList();
+        songList.setTitle(title);
+        songList.setPic(pic);
+        songList.setIntroduction(introduction);
+        songList.setStyle(style);
+        boolean flag = songListService.insert(songList);
+        if (flag) { // 保存成功
+            jsonObject.put(Consts.CODE, 1);
+            jsonObject.put(Consts.MSG, "添加成功");
+            return jsonObject;
+        }
+        jsonObject.put(Consts.CODE, 0);
+        jsonObject.put(Consts.MSG, "添加失败");
+        return jsonObject;
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public Object updateSongList(HttpServletRequest request) { // 修改歌单
+        JSONObject jsonObject = new JSONObject();
+        String id = request.getParameter("id").trim(); // 主键
+        String title = request.getParameter("title").trim(); // 标题
+        String introduction = request.getParameter("introduction").trim(); // 简介
+        String style = request.getParameter("style").trim(); // 风格
+
+        // 保存到歌单的对象中
+        SongList songList = new SongList();
+        songList.setId(Integer.parseInt(id));
+        songList.setTitle(title);
+        songList.setIntroduction(introduction);
+        songList.setStyle(style);
+        boolean flag = songListService.update(songList);
+        if (flag) { // 保存成功
+            jsonObject.put(Consts.CODE, 1);
+            jsonObject.put(Consts.MSG, "修改成功");
+            return jsonObject;
+        }
+        jsonObject.put(Consts.CODE, 0);
+        jsonObject.put(Consts.MSG, "修改失败");
+        return jsonObject;
+    }
+
+
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public Object deleteSongList(HttpServletRequest request) { // 删除歌单
+        String id = request.getParameter("id").trim(); // 主键
+        boolean flag = songListService.delete(Integer.parseInt(id));
+        return flag;
+    }
+
+    @RequestMapping(value = "/selectByPrimaryKey", method = RequestMethod.GET)
+    public Object selectByPrimaryKey(HttpServletRequest request) { // 根据主键查询整个对象
+        String id = request.getParameter("id").trim(); // 主键
+        return songListService.selectByPrimaryKey(Integer.parseInt(id));
+    }
+
+    @RequestMapping(value = "/allSongList", method = RequestMethod.GET)
+    public Object allSongList(HttpServletRequest request) { // 查询所有歌单
+        return songListService.allSongList();
+    }
+
+    @RequestMapping(value = "/songListOfTitle", method = RequestMethod.GET)
+    public Object songListOfName(HttpServletRequest request) { // 根据标题名字模糊查询列表
+        String title = request.getParameter("title").trim(); //歌单标题
+        return songListService.songListOfTitle(title);
+    }
+
+    @RequestMapping(value = "/likeTitle", method = RequestMethod.GET)
+    public Object likeTitle(HttpServletRequest request) { // 根据标题模糊查询歌单列表
+        String title = request.getParameter("title").trim(); // 歌单标题
+        return songListService.likeTitle("%" + title + "%");
+    }
+
+    @RequestMapping(value = "/likeStyle", method = RequestMethod.GET)
+    public Object likeStyle(HttpServletRequest request) { // 根据风格模糊查询歌单列表
+        String style = request.getParameter("style").trim(); // 歌单风格
+        return songListService.likeTitle("%" + style + "%");
+    }
+
+    @RequestMapping(value = "/updateSongListPic", method = RequestMethod.POST)
+    public Object updateSongListPic(@RequestParam("file") MultipartFile avatorFile, @RequestParam("id") int id) { // 更新歌手图片
+        JSONObject jsonObject = new JSONObject();
+        if (avatorFile.isEmpty()) {
+            jsonObject.put(Consts.CODE, 0);
+            jsonObject.put(Consts.MSG, "文件上传失败");
+            return jsonObject;
+        }
+        //文件名=当前时间到毫秒+原来的文件名
+        String fileName = System.currentTimeMillis() + avatorFile.getOriginalFilename();
+        //文件路径
+        String filePath = System.getProperty("user.dir") + System.getProperty("file.separator") + "img"
+                + System.getProperty("file.separator") + "songListPic";
+        //如果文件路径不存在，新增该路径
+        File file1 = new File(filePath);
+        if (!file1.exists()) {
+            file1.mkdir();
+        }
+        //实际的文件地址
+        File dest = new File(filePath + System.getProperty("file.separator") + fileName);
+        //存储到数据库里的相对文件地址
+        String storeAvatorPath = "/img/songListPic/" + fileName;
+        try {
+            avatorFile.transferTo(dest);
+            SongList songList = new SongList();
+            songList.setId(id);
+            songList.setPic(storeAvatorPath);
+            boolean flag = songListService.update(songList);
+            if (flag) {
+                jsonObject.put(Consts.CODE, 1);
+                jsonObject.put(Consts.MSG, "上传成功");
+                jsonObject.put("pic", storeAvatorPath);
+                return jsonObject;
+            }
+            jsonObject.put(Consts.CODE, 0);
+            jsonObject.put(Consts.MSG, "上传失败");
+            return jsonObject;
+        } catch (IOException e) {
+            jsonObject.put(Consts.CODE, 0);
+            jsonObject.put(Consts.MSG, "上传失败" + e.getMessage());
+        } finally {
+            return jsonObject;
+        }
+    }
+}
