19ac58270ba17582a5a7411439fb2d2f2185df86
HelloZXB <Hello16369@163.com>
2020-11-07 09:24:14 +0800

    收藏-service层

---
+++ src/main/java/com/javaclimb/music/service/CollectService.java
@@ -0,0 +1,36 @@
+package com.javaclimb.music.service;
+
+import com.javaclimb.music.domain.Collect;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 收藏service接口
+ */
+public interface CollectService {
+    /**
+     * 增加
+     */
+    public boolean insert(Collect collect);
+
+    /**
+     * 删除
+     */
+    public boolean delete(Integer id);
+
+    /**
+     * 查询所有收藏
+     */
+    public List<Collect> allCollect();
+
+    /**
+     * 查询某个用户的收藏列表
+     */
+    public List<Collect> collectOfUserId(Integer userId);
+
+    /**
+     * 查询某个用户是否已经收藏了某个歌曲
+     */
+    public boolean existSongId(@Param("userId") Integer userId, @Param("songId") Integer songId);
+}
